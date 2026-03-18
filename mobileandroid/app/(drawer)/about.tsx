2e41fc71ba050a0c14b51fdf5c9628e447257728
Umut <umut.clsc01@outlook.com>
2026-03-18 22:26:05 +0300

    about page

---
+++ mobileandroid/app/(drawer)/about.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+
+
+export default function About(){
+    return(
+        <>
+        <View>
+            <Text>Hello</Text>
+            </View>
+            </>
+    );
+}
+
